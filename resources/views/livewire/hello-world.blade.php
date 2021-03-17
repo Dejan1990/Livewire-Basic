958732ee3be1b0ef823fa960b9fe54b2537f1609
dejan1990 <dejan.simo@hotmail.com>
2021-03-17 12:00:18 +0100

    Data Binding

--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,3 +1,11 @@
 <div>
-    Hello {{ $name }}
+    <input wire:model="name" type="text"> <!-- U pozadini se odvija AJAX request -->
+    <input wire:model="loud" type="checkbox">
+    <select wire:model=greeting multiple>
+        <option>Hello</option>
+        <option>Goodbay</option>
+        <option>Adios</option>
+    </select>
+
+    {{ implode(', ',$greeting) }} {{ $name }} @if ($loud) ! @endif
 </div>
