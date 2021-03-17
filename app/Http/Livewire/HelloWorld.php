33f8ca18b9810c1b4b9a02a9fb0843796dce1839
dejan1990 <dejan.simo@hotmail.com>
2021-03-17 13:41:38 +0100

    Lifecycle Hooks

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -2,14 +2,27 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 class HelloWorld extends Component
 {
-    public $name = 'Jelly';
-    public $loud = false;
-    public $greeting = ['Hello']; 
-    
+    public $name = 'Jelly', $hydrate;
+
+    public function mount($name) // Lifecycle Hooks
+    {
+        $this->name = $name; // ovaj metod se odvija sa prvim request-om, it's gonna be the first thing that happens when this component is boot up
+    }
+
+    public function hydrate() // Lifecycle Hooks -> is gonna run at the begining of every subsequent request that'll livewire component makes
+    {
+        $this->hydrate = 'I am from Hydrate';
+    }
+
+    public function updatedName() // Lifecycle Hooks -> is gonna run any time you update a public property
+    {
+        $this->name = strtoupper($this->name);
+    }
 
     public function render()
     {
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,15 +1,7 @@
 <div>
     <input wire:model="name" type="text"> <!-- U pozadini se odvija AJAX request -->
-    <input wire:model="loud" type="checkbox">
-    <select wire:model=greeting multiple>
-        <option>Hello</option>
-        <option>Goodbay</option>
-        <option>Adios</option>
-    </select>
 
-    {{ implode(', ',$greeting) }} {{ $name }} @if ($loud) ! @endif
+    Hello {{ $name }} 
 
-    <form action="#" wire:submit.prevent="$set('name', 'Bingo')">
-        <button>Reset Name</button> <!-- wire: -> Ovde mozemo ubaciti bilo koji event -->
-    </form>
+    <h2>{{ $hydrate }}</h2>
 </div>
